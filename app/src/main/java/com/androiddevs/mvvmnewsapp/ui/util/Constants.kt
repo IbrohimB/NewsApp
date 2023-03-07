3310e64aba056733e2cdac03431b86376404f213
IbrohimB <botirovibrohim@gmail.com>
2023-03-07 18:58:30 +0500

    DAO, MVVM

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object{
+       const val API_KEY = "8ba134e913204baf9199ea0541731462"
+        const val BASE_URL= "https://newsapi.org"
+    }
+}
